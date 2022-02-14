8e2d0b677e9ddf9ea3c0026a15405154f7442c57
Anhad <84476084+07anhad@users.noreply.github.com>
2022-02-14 18:21:40 +0530

    bruh

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,5 +9,14 @@ navToggle.addEventListener("click", () => {
 navClose.addEventListener("click", () => {
 	navMenu.classList.remove("show-menu");
 });
+
+/* CHANGE BACKGROUND HEADER =======================*/
+function scrollHeader() {
+	const header = document.querySelector("header");
+
+	if (this.scrollY >= 50) header.classList.add("scroll-header");
+	else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
    
 
